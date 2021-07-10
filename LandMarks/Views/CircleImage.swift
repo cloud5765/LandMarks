ebbd6a590b350900e8eef7ff33ccb73d8850c69b
Dillon <dc2513@uncw.edu>
2021-07-10 14:40:32 -0400

    Submitting first swiftUI Landmarks App

---
+++ LandMarks/Views/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  LandMarks
+//
+//  Created by Dillon Cloud on 7/10/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
---
+++ LandMarks/Views/LandmarkList.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkList.swift
+//  LandMarks
+//
+//  Created by Dillon Cloud on 7/10/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
