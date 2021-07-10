4610e13dfbf09db94fbc74219b4ce9824feef00b
Dillon <dc2513@uncw.edu>
2021-07-10 00:20:46 -0400

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Dillon Cloud on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
